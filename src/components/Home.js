584929150aa97cf46a2bba28723b46d53541b0d6
Miikka Tulimäki <miikkat@me.com>
2016-09-25 17:55:00 +0300

    Make area picker visible only when needed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 import AreaPicker from '../components/AreaPicker';
 import DatePicker from '../components/DatePicker';
@@ -8,6 +8,9 @@ import MovieList from '../components/MovieList';
 import theatreAreaConverter from '../converters/theatreArea';
 
 const styles = StyleSheet.create({
+  areaTitle: {
+    fontSize: 18,
+  },
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
@@ -37,7 +40,9 @@ class Home extends Component {
         year: '1982',
       },
     ],
-    persistedTheaterID: '1021',
+    persistedTheaterID: '1029',
+    persistedTheaterName: 'Valitse alue/teatteri',
+    showAreaPicker: false,
   };
 
   componentDidMount() {
@@ -61,18 +66,32 @@ class Home extends Component {
   }
 
   handleAreaChange = (id) => {
-    this.setState({ persistedTheaterID: id });
+    this.setState({
+      persistedTheaterID: id,
+      persistedTheaterName: this.state.theatres.find(x => x.ID === id).Name,
+      showAreaPicker: false,
+    });
+  }
+
+  toggleAreaPicker = () => {
+    this.setState({
+      showAreaPicker: !this.state.showCancel,
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Cinemabot</Text>
-        <AreaPicker
-          onValueChange={this.handleAreaChange}
-          persistedTheaterID={this.state.persistedTheaterID}
-          theatres={this.state.theatres}
-        />
+        <TouchableHighlight onPress={this.toggleAreaPicker}>
+          <Text style={styles.areaTitle}>{this.state.persistedTheaterName}</Text>
+        </TouchableHighlight>
+        {this.state.showAreaPicker ?
+          <AreaPicker
+            onValueChange={this.handleAreaChange}
+            persistedTheaterID={this.state.persistedTheaterID}
+            theatres={this.state.theatres}
+          /> : <View />}
         <DatePicker />
         <MovieList movies={this.state.movies} />
       </View>
