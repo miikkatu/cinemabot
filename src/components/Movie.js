3abadbc958ec566227b5093cdbf7d6884ae973da
Miikka Tulimäki <miikkat@me.com>
2016-10-02 15:27:52 +0300

    Add detailed movie info

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,31 +1,129 @@
 import React, { Component, PropTypes } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import moment from 'moment';
+
+require('moment/locale/fi');
 
 const styles = StyleSheet.create({
   container: {
     margin: 20,
   },
+  detail: {
+    fontSize: 14,
+  },
+  detailBold: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
   link: {
     color: 'blue',
+  },
+  originalTitle: {
+    fontSize: 14,
+    fontStyle: 'italic',
+  },
+  section: {
+    paddingBottom: 10,
   },
   title: {
     fontSize: 20,
   },
-  year: {
-    fontSize: 10,
-  },
 });
 
 export default class Movie extends Component {
+  getActorList = () => {
+    const actors = this.props.movie.Cast.Actor.map((actor) => {
+      return ` ${actor.FirstName} ${actor.LastName}`;
+    });
+    return actors.toString().slice(1);
+  }
+
+  getDirectorList = () => {
+    if (this.props.movie.Directors.Director.constructor === Array) {
+      const directors = this.props.movie.Directors.Director.map((director) => {
+        return ` ${director.FirstName} ${director.LastName}`;
+      });
+      return directors.toString().slice(1);
+    }
+    else {
+      return ` ${this.props.movie.Directors.Director.FirstName} ${this.props.movie.Directors.Director.LastName}`;
+    }
+  }
+
+  getDuration = () => {
+    const m = moment.duration(parseInt(this.props.movie.LengthInMinutes, 10), 'minutes');
+    return `${m.hours()} h ${m.minutes()} min`;
+  }
+
+  setUriAsHttps = () => {
+    return this.props.movie.Images.EventSmallImagePortrait
+      .replace(/^http:\/\//i, 'https://');
+  }
+
   render() {
     const goToHome = () => Actions.pop();
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>{this.props.movie.Title}</Text>
-        <Text style={styles.year}>{this.props.movie.ProductionYear}</Text>
-        <Text style={styles.link} onPress={goToHome}>Go back</Text>
-      </View>
+      <ScrollView style={styles.container}>
+
+        <Image
+          style={{width: 99, height: 146}}
+          source={{ uri: this.setUriAsHttps() }}
+        />
+
+        <View style={styles.section}>
+          <Text style={styles.title}>
+            {this.props.movie.Title}
+          </Text>
+
+          <Text style={styles.originalTitle}>
+            {this.props.movie.OriginalTitle} ({this.props.movie.ProductionYear})
+          </Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.detail}>
+            Lajityyppi: {this.props.movie.Genres}
+          </Text>
+
+          <Text style={styles.detail}>
+            Levittäjä: {this.props.movie.LocalDistributorName}
+          </Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.detail}>
+            Ohjaus: <Text>{this.getDirectorList()}</Text>
+          </Text>
+
+          <Text style={styles.detail}>
+            Pääosissa: {this.getActorList()}
+          </Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.detail}>
+            Ensi-ilta: <Text style={styles.detailBold}>
+              {moment(this.props.movie.dtLocalRelease).format('D.M.YYYY')}</Text>
+          </Text>
+
+          <Text style={styles.detail}>
+            Kesto: <Text style={styles.detailBold}>{this.getDuration()}</Text>
+          </Text>
+
+          <Text style={styles.detail}>Ikäraja: <Text style={styles.detailBold}>
+            {this.props.movie.RatingLabel}</Text>
+          </Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.detail}>
+            {this.props.movie.Synopsis}
+          </Text>
+        </View>
+
+        <Text style={styles.link} onPress={goToHome}>Takaisin</Text>
+      </ScrollView>
     );
   }
 }
